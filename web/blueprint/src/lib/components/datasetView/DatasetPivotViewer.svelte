758dd36ef6053b69c8cfb0bd7ad83e253ab959ee
Nikhil Thorat <nsthorat@gmail.com>
2024-01-24 11:01:36 -0500

    Add more datasets.

--- web/blueprint/src/lib/components/datasetView/DatasetPivotViewer.svelte
+++ web/blueprint/src/lib/components/datasetView/DatasetPivotViewer.svelte
@@ -1,35 +1,35 @@
 <script lang="ts">
   import {
-    DATASETS_TAG,
-    queryDatasetSchema,
-    querySelectRows,
-    querySelectRowsSchema
+      DATASETS_TAG,
+      queryDatasetSchema,
+      querySelectRows,
+      querySelectRowsSchema
   } from '$lib/queries/datasetQueries';
-  import {createApiQuery} from '$lib/queries/queryUtils';
+  import { createApiQuery } from '$lib/queries/queryUtils';
   import {
-    getDatasetViewContext,
-    getSelectRowsOptions,
-    getSelectRowsSchemaOptions
+      getDatasetViewContext,
+      getSelectRowsOptions,
+      getSelectRowsSchemaOptions
   } from '$lib/stores/datasetViewStore';
-  import {datasetLink} from '$lib/utils';
-  import {getDisplayPath, getSearchHighlighting} from '$lib/view_utils';
+  import { datasetLink } from '$lib/utils';
+  import { getDisplayPath, getSearchHighlighting } from '$lib/view_utils';
   import {
-    DatasetsService,
-    ROWID,
-    childFields,
-    deserializePath,
-    isSignalField,
-    serializePath,
-    type Path,
-    type PivotResult
+      DatasetsService,
+      ROWID,
+      childFields,
+      deserializePath,
+      isSignalField,
+      serializePath,
+      type Path,
+      type PivotResult
   } from '$lilac';
-  import {Search, SkeletonText} from 'carbon-components-svelte';
+  import { Search, SkeletonText } from 'carbon-components-svelte';
   import type {
-    DropdownItem,
-    DropdownItemId
+      DropdownItem,
+      DropdownItemId
   } from 'carbon-components-svelte/types/Dropdown/Dropdown.svelte';
   import DropdownPill from '../common/DropdownPill.svelte';
-  import DatasetPivotResult, {type OuterPivot} from './DatasetPivotResult.svelte';
+  import DatasetPivotResult, { type OuterPivot } from './DatasetPivotResult.svelte';
 
   let outerLeafPath: Path | undefined = undefined;
   let innerLeafPath: Path | undefined = undefined;
@@ -219,7 +219,7 @@
       <div class="mx-20 mt-8 w-full text-lg text-gray-600">Select a field to explore.</div>
     {:else if outerLeafPath == null}
       <div class="mx-20 mt-8 w-full text-lg text-gray-600">Select a field to group by.</div>
-    {:else if groups == null}
+    {:else if groups == null || numRowsInQuery == null}
       <SkeletonText />
     {:else}
       <div class="flex w-full flex-col gap-y-10">
@@ -250,26 +250,7 @@
                   {group.count.toLocaleString()} rows
                 </span>
               </div>
-              <div class="">
-                <a class="flex flex-row" href={groupLink} target="_blank">
-                  Explore
-                  <svg
-                    class="ms-2 h-3 w-3 rtl:rotate-[270deg]"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 18 18"
-                  >
-                    <path
-                      stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778"
-                    />
-                  </svg>
-                </a>
-              </div>
+              <a class="flex flex-row" href={groupLink}> <button>Explore</button></a>
             </div>
 
             {#if outerLeafPath && innerLeafPath && numRowsInQuery}
